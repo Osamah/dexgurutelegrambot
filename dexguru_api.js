5f3745634e8d7d7e7ef78bd0c9f8644bbe605823
Osamah <osamah.bukraa@twenty5.be>
2022-03-21 00:06:16 +0100

    Create telegram bot, add dexguru api logic, create price command

---
+++ dexguru_api.js
@@ -0,0 +1,24 @@
+require('isomorphic-fetch');
+require('dotenv');
+
+const API_KEY = process.env.DEXGURU_API_KEY || '';
+const BASEURL = 'https://api.dev.dex.guru/v1/';
+const API_KEY_SUFFIX = `?api-key=${API_KEY}`;
+
+async function getTokenData(chain_id, token_address) {
+
+    const response = await fetch(`${BASEURL}chain/${chain_id}/tokens/${token_address}/market${API_KEY_SUFFIX}`);
+    if (response.status >= 400) {
+        console.log(response);
+        return {
+            error: true,
+            status: 404
+        }
+    }
+    const data = await response.json();
+    return data;
+}
+
+module.exports = {
+    getTokenData
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const API = require('./dexguru_api');
+
+const token = process.env.TELEGRAM_BOT_TOKEN || '';
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(
+        chatId,
+        '*Welcome to the The Dex Guru Bot! 🤖*\n\nYou can use this bot to track prices, volume, set liquidity alerts, trading volume alerts and much more!\n\nIf you wish to get more information on everything I can do, feel free to use the /help command',
+        {parse_mode: 'Markdown'}
+    );
+});
+
+bot.onText(/\/help/, (msg, match) => {
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(
+        chatId,
+        'Overview of all built-in features\n\n*TRACKING*\n/price _tokenaddress_ - Shows data about a given token, only ETH chain supported for now\n/p _tokenaddress_ - Shorthand for the price command, can also be used without argument to track default token\n\n*ALERTING*\n_No commands yet...😔_',
+        {parse_mode: 'Markdown'}
+    );
+});
+
+bot.onText(/\/(p|price) (.+)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const token_address = match[2];
+
+    try {
+        const data = await API.getTokenData(1, token_address);
+        console.log('data', data);
+
+        if (data.error) {
+            if (data.status == 404) {
+                bot.sendMessage(chatId, 'Token not found, please enter a valid token address:\n`/p token_address`', {'parse_mode': 'MarkdownV2'});
+            } else {
+                bot.sendMessage(chatId, 'Something went wrong, please enter a valid token address:\n`/p token_address`', {'parse_mode': 'MarkdownV2'});
+            }
+        } else {
+            bot.sendMessage(chatId, `
+Token: _${token_address}_
+Price: *$${data.price_usd.toFixed(2)} (${data.price_24h_delta_usd > 0 ? '⬆️' : '⬇️'} ${(data.price_24h_delta_usd * 100).toFixed(2)}%)*
+Volume 24 hour: *$${data.volume_24h_usd.toFixed(2)}*
+Liquidity: *$${data.liquidity_usd.toFixed(2)}*
+`,
+    {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true,
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'View graph',
+                        url: `https://dex.guru/token/${token_address}-eth`
+                    },
+                    {
+                        text: 'Etherscan',
+                        url: `https://etherscan.io/token//${token_address}-eth`
+                    }
+                ]
+            ]
+        }
+    });
+        }
+    } catch (e) {
+
+    }
+});
+
+bot.onText(/\/bruh/, (msg, match) => {
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(chatId, 'BRUH');
+});
+
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+
+// });
